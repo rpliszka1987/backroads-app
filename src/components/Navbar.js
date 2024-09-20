52d353f2c11df55471cebee405825ddc576c890b
Robert Pliszka <robplisz@gmail.com>
2024-09-19 20:11:31 -0400

    Fixed the links in the navbar to be generated from the data.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
-import logo from '../images/logo.svg'
+import logo from '../images/logo.svg';
+import { pageLinks } from '../data';
 
 const Navbar = () => {
   return (
@@ -10,35 +11,17 @@ const Navbar = () => {
             <i className="fas fa-bars"></i>
           </button>
         </div>
-
+        {console.log(pageLinks)}
         <ul className="nav-links" id="nav-links">
-          <li>
-            <a href="#home" className="nav-link">
-              {' '}
-              home{' '}
-            </a>
-          </li>
-
-          <li>
-            <a href="#about" className="nav-link">
-              {' '}
-              about{' '}
-            </a>
-          </li>
-
-          <li>
-            <a href="#services" className="nav-link">
-              {' '}
-              services{' '}
-            </a>
-          </li>
-
-          <li>
-            <a href="#tours" className="nav-link">
-              {' '}
-              tours
-            </a>
-          </li>
+          {pageLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a href={link.href} className="nav-link">
+                  {link.text}
+                </a>
+              </li>
+            );
+          })}
         </ul>
 
         <ul className="nav-icons">
