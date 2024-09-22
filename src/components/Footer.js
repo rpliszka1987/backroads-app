224cd40d33d8c94dc1fd1c62fc89b2d4a5631b08
Robert Pliszka <robplisz@gmail.com>
2024-09-22 13:42:45 -0400

    Restructured Footer component links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,19 @@
-import { socialLinks } from '../data';
+import { pageLinks, socialLinks } from '../data';
 
 const Footer = () => {
   return (
     <footer className="section footer">
       <ul className="footer-links">
-        <li>
-          <a href="#home" className="footer-link">
-            home
-          </a>
-        </li>
-        <li>
-          <a href="#about" className="footer-link">
-            about
-          </a>
-        </li>
-        <li>
-          <a href="#services" className="footer-link">
-            services
-          </a>
-        </li>
-        <li>
-          <a href="#featured" className="footer-link">
-            featured
-          </a>
-        </li>
+        {pageLinks.map((link) => {
+          const { id, href, text} = link;
+          return (
+            <li key={id}>
+              <a href={href} className="footer-link">
+                {text}
+              </a>
+            </li>
+          );
+        })}
       </ul>
       <ul className="footer-icons">
         {socialLinks.map((link) => {
