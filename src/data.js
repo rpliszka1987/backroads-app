b6704d55ae613b8f82d07596ea872fe113663d86
Robert Pliszka <robplisz@gmail.com>
2024-09-22 13:38:23 -0400

    Added 2 more locations to the tours data

--- src/data.js
+++ src/data.js
@@ -2,6 +2,8 @@ import tour1 from './images/tour-1.jpeg';
 import tour2 from './images/tour-2.jpeg';
 import tour3 from './images/tour-3.jpeg';
 import tour4 from './images/tour-4.jpeg';
+import tour5 from './images/tour-5.jpeg';
+import tour6 from './images/tour-6.jpeg';
 
 export const pageLinks = [
   {
@@ -84,5 +86,25 @@ export const tours = [
     location: 'kenya',
     duration: 20,
     cost: 3300,
+  },
+  {
+    id: 5,
+    image: tour5,
+    date: 'february 14th, 2020',
+    title: 'south africa',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+    location: 'africa',
+    duration: 15,
+    cost: 3500,
+  },
+  {
+    id: 6,
+    image: tour6,
+    date: 'december 5th, 2020',
+    title: 'india highlights',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.',
+    location: 'india',
+    duration: 10,
+    cost: 2500,
   },
 ];
